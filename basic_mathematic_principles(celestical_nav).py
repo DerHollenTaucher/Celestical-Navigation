0f69880bfaca5d2cc24c6851d86b4ccb49ef4279
Yuxian Yuan <derhollentaucher@outlook.com>
2026-04-23 08:07:13 +0800

    Update basic_mathematic_principles(celestical_nav).py

--- basic_mathematic_principles(celestical_nav).py
+++ basic_mathematic_principles(celestical_nav).py
@@ -2,3 +2,10 @@
 from skyfield.api import load, Star, Topos
 from scripy.optimize import minimize
 import numpyas np
+
+#===
+#input
+
+#UTC time
+year, month, day = 2026,4, 22  #yy,mm, dd
+hour, minute, second = 12,0, 0  #hh, mm, ss
